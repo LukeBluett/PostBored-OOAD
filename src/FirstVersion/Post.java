cb354ad61c44be2adca9c89a141e273d9c5629bc
lbluett <lukebluett@mail.com>
2015-12-11 09:58:06 +0000

    Add Constructors and accessors and mutators

--- src/FirstVersion/Post.java
+++ src/FirstVersion/Post.java
@@ -1,7 +1,67 @@
 package FirstVersion;
 
+import java.util.Date;
+
 /**
- * Created by lbluett on 11/12/15.
+ *
  */
 public class Post {
+
+    private int id;
+    private String title;
+    private String text;
+    private String tag;
+    private Date postTime;
+
+    public Post() {
+
+    }
+
+    public Post(int id, String title, String text, String tag, Date postTime) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.tag = tag;
+        this.postTime = postTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Date getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(Date postTime) {
+        this.postTime = postTime;
+    }
 }
--- src/FirstVersion/PostCollection.java
+++ src/FirstVersion/PostCollection.java
@@ -1,7 +1,24 @@
 package FirstVersion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by lbluett on 11/12/15.
+ *
  */
 public class PostCollection {
+
+    private List<Post> Posts;
+
+    public PostCollection() {
+        Posts = new ArrayList<Post>();
+    }
+
+    public Post getPost(int i) {
+        return Posts.get(i);
+    }
+
+    public void addPost(Post post) {
+        Posts.add(post);
+    }
 }
