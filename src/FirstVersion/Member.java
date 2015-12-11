3630b207f4e1efd9758f14555002eab8b45638b1
lbluett <lukebluett@mail.com>
2015-12-11 09:57:30 +0000

    Add Constructors and accessors and mutators

--- src/FirstVersion/Member.java
+++ src/FirstVersion/Member.java
@@ -1,7 +1,58 @@
 package FirstVersion;
 
 /**
- * Created by lbluett on 11/12/15.
+ *
  */
 public class Member {
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public Member() {
+        this.id = 0;
+        this.username = "";
+        this.email = "";
+        this.password = "";
+    }
+
+    public Member(String memberUsername, String memberEmail, String memberPassword) {
+        this.id = id;
+        this.username = memberUsername;
+        this.email = memberEmail;
+        this.password = memberPassword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
